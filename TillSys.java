087d177147e2a4371c9bcd94bf2d6e101b03c638
Erwin Suarez <t00189596@365s.ittralee.ie>
2016-11-15 12:04:16 +0000

    Added main classes

---
+++ TillSys.java
@@ -0,0 +1,15 @@
+/**
+ * @(#)TillSys.java
+ *
+ *
+ * @Erwin Suarez
+ * @version 1.00 2016/11/15
+ */
+ 
+public class TillSys {
+	
+	public static void main(String[] args){
+		
+		TillSysWindow mainWindow = new TillSysWindow();
+    }       
+}
