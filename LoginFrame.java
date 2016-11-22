eea7717cf5a0679bc416106fad0e636ce16dabf8
Erwin Suarez <t00189596@365s.ittralee.ie>
2016-11-22 15:55:33 +0000

    Added login frames

---
+++ LoginFrame.java
@@ -0,0 +1,24 @@
+/**
+ * Created by t00189596 on 22/11/2016.
+ */
+import javax.swing.JFrame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class LoginFrame extends JFrame {
+    public LoginFrame (TillSys parent) {
+        setSize(400, 400);
+        setTitle("Login");
+
+        parent.setVisible(false);
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                parent.setVisible(true);
+
+                LoginFrame.this.dispose();
+            }
+        });
+    }
+}
--- TillSys.java
+++ TillSys.java
@@ -5,26 +5,119 @@
  * @version 1.00 2016/11/15
  */
 import javax.swing.*;
- 
-public class TillSys extends JFrame {
-	
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TillSys extends JFrame implements ActionListener{
+
+    JMenu fileMenu;
+
+    //main driver
 	public static void main(String[] args){
 		
-		TillSys frame = new TillSys();
-    }
+		TillSys winOne = new TillSys();
+        winOne.setVisible(true);
+    }//end driver
 
-    //Constructor creates the main windo
+    //Constructor creates the main window
     public TillSys () {
-        setTitle("Still System");
-        setSize(600, 600);
-        setLocationRelativeTo(null);
-        MainContainer mainContainer = new MainContainer();
-        Buttons buttons = new Buttons();
+
+        Container contents;
+
+        //this will set the frame properties
+        setTitle("Till System");
+        setSize(640, 400);
+        setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
 
-        add(mainContainer);
-        add(buttons);
+        //this will place the frame centered in the screen
+        //copied from web in File
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
+
+        contents = getContentPane();
+        contents.setLayout(new FlowLayout());
+
+        loginFileMenu();
+
+        JButton loginButton = new JButton("Login");
+        loginButton.setSize(100, 50);
+
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+        menuBar.setBackground(Color.lightGray);
+        menuBar.add(fileMenu);
+    }
+
+    public void actionPerformed(ActionEvent event){
+        String menuName;
+        menuName = event.getActionCommand();
+        if (menuName.equals("Quit")) {
+            System.exit(0);
+        } else if (menuName.equals("Member")) {
+            // show input screen for member
+            LoginFrame memberLogin = new LoginFrame(TillSys.this);
+            memberLogin.setVisible(true);
+        } else if (menuName.equals("Administrator")) {
+            // show input screen for administrator
+            LoginFrame adminLogin = new LoginFrame(TillSys.this);
+            adminLogin.setVisible(true);
+        }
+
+        System.out.println(menuName);
 
     }
+
+    private void loginFileMenu(){
+
+        JMenuItem item;
+
+        fileMenu = new JMenu("Login");
+
+        item = new JMenuItem("Administrator");
+        item.addActionListener(this);
+        fileMenu.add(item);
+
+
+
+        item = new JMenuItem("Member");
+        item.addActionListener(this);
+        fileMenu.add(item);
+
+        item = new JMenuItem("Quit");
+        item.addActionListener(this);
+        fileMenu.add(item);
+    }
+
+    private static void placeComponents(JPanel panel) {
+
+        panel.setLayout(null);
+
+        JLabel userLabel = new JLabel("User");
+        userLabel.setBounds(10, 10, 80, 25);
+        panel.add(userLabel);
+
+        JTextField userText = new JTextField(20);
+        userText.setBounds(100, 10, 160, 25);
+        panel.add(userText);
+
+        JLabel passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(10, 40, 80, 25);
+        panel.add(passwordLabel);
+
+        JPasswordField passwordText = new JPasswordField(20);
+        passwordText.setBounds(100, 40, 160, 25);
+        panel.add(passwordText);
+
+        JButton loginButton = new JButton("login");
+        loginButton.setBounds(10, 80, 80, 25);
+        panel.add(loginButton);
+
+        JButton registerButton = new JButton("register");
+        registerButton.setBounds(180, 80, 80, 25);
+        panel.add(registerButton);
+    }
+
 }
